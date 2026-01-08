bd3ddbade2107f363ba4d3d11571f90a0fc6e400
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-01-08 22:00:32 +1100

    done with sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    number_list = []
+    if length > 0:
+        number_list.append(0)
+    if length >= 2:
+        number_list.append(1)
+        for i in range(2, length):
+            number_list.append(number_list[-1] + number_list[-2])
+    print(number_list)
